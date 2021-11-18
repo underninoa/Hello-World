394d7e59a2ce004c065c46d4838eca14db35186f
USER <USER@10.129.241.42>
2021-11-18 14:59:23 +0900

    input args values

--- hello world/src/OkJavaGoInHome.java
+++ hello world/src/OkJavaGoInHome.java
@@ -1,8 +1,6 @@
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import javax.swing.JOptionPane;
 
-import javax.swing.*;
-
+import org.opentutorials.iot.DimmingLights;
 import org.opentutorials.iot.Elevator;
 import org.opentutorials.iot.Lighting;
 import org.opentutorials.iot.Security;
@@ -10,7 +8,11 @@ import org.opentutorials.iot.Security;
 public class OkJavaGoInHome {
 	public static void main(String[] args) {
 		
-		String id = "JAVA APT 507";
+		String id = args[0];
+		String bright = args[1];
+		
+		//String id = JOptionPane.showInputDialog("Enter a value");
+		//String bright = JOptionPane.showInputDialog("Enter a Bright level");
 		
 		// Elevator call
 		Elevator myElevator = new Elevator(id);
@@ -20,12 +22,15 @@ public class OkJavaGoInHome {
 		Security mySecurity = new Security(id);
 		mySecurity.off();
 		
-		// Light on
+		// Light on,off
 		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
 		hallLamp.on();
 		
 		Lighting floorLamp = new Lighting(id+" / floor Lamp");
 		floorLamp.on();
-	
+		
+		DimmingLights moodLamp = new DimmingLights(id+" mmdLamp");
+		moodLamp.setBright(Double.parseDouble(bright));
+		moodLamp.on();
 	}
 }
